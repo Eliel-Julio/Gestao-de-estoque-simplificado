0b7d97d17366a8247d800b6d98a3b4d715732a49
Eliel-Julio <elieljuliosoaresdasilva@gmail.com>
2025-04-22 15:02:12 -0300

    .

--- Projeto/CRUD.py
+++ Projeto/CRUD.py
@@ -1,11 +1,16 @@
 from def_database import *
 
 
-for i in range(10):
-    item = Produto(nome=f'Produto {i}', quant=100, precoVenda=1000, precoCusto=200)
-    session.add(item)
-    session.commit()
+# for i in range(10):
+#     item = Produto(nome=f'Produto {i}', quant=100, precoVenda=1000, precoCusto=200)
+#     session.add(item)
+#     session.commit()
 
 # item = Produto(nome="Iphone", quant=100, precoVenda=5000, precoCusto=200)
 # session.add(item)
 # session.commit()
+
+
+produto = session.query(Produto).filter_by(id=1).first()
+produto.Baixa(1)
+session.commit()
--- Projeto/front/app.py
+++ Projeto/front/app.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
-from flask import Flask,render_template
+from flask import Flask,render_template, redirect, url_for
 from def_database import *
 
 app = Flask(__name__)
@@ -12,11 +12,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', produtos=session.query(Produto).all())
 @app.route('/produto/<int:id>')
-def produto():
+def produto(id):
     produto = session.query(Produto).filter_by(id=id).first()
     if produto:
+        print(produto.nome)
         return render_template('produto.html', produto=produto)
     else:
         return "Produto não encontrado", 404
+
+@app.route('/produto/<int:id>/update/baixa/<int:quant>', methods=['POST'])
+def baixa_produto(id, quant):
+    produto = session.query(Produto).filter_by(id=id).first()
+    produto.Baixa(quant)
+    session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/produto/<int:id>/update/subir/<int:quant>', methods=['POST'])
+def subir_produto(id, quant):
+    produto = session.query(Produto).filter_by(id=id).first()
+    produto.Subir(quant)
+    session.commit()
+    return redirect(url_for('index'))
 
 app.run(debug=True)
